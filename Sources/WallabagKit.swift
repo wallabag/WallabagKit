63a13ce893957b0334855480bf7a7db1d7c9f571
Marinel maxime <maxime.m62@gmail.com>
2019-06-07 16:58:59 +0200

    Reboot WallabagKit

---
+++ Sources/WallabagKit.swift
@@ -0,0 +1,9 @@
+//
+//  WallabagKit.swift
+//  wallabag
+//
+//  Created by Marinel maxime on 07/06/2019.
+//  Copyright © 2019 wallabag. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/WallabagKitTests/WallabagKitTests.swift
@@ -0,0 +1,23 @@
+//
+//  WallabagKitTests.swift
+//  wallabag
+//
+//  Created by Marinel maxime on 07/06/2019.
+//  Copyright © 2019 wallabag. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import WallabagKit
+
+class WallabagKitTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(WallabagKit().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
